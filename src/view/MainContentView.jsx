4ec5e707ed1e0232968e9c40294d455944543127
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2019-11-24 10:43:21 +0530

    I know what I did !!!!

--- src/view/MainContentView.jsx
+++ src/view/MainContentView.jsx
@@ -10,6 +10,7 @@ import Box from "@material-ui/core/Box";
 import Copyright from "../component/Copyright";
 import {useStyles} from "../style/MainContentStyle";
 import clsx from "clsx";
+import VehicleTable from "../component/VehicleTable";
 
 export default function () {
     const classes = useStyles();
@@ -26,6 +27,18 @@ export default function () {
                         </Paper>
                     </Grid>
 
+                    {/* Recent Orders */}
+                    <Grid item xs={12}>
+                        <VehicleTable />
+                    </Grid>
+
+                    {/* Recent Orders */}
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <Orders />
+                        </Paper>
+                    </Grid>
+
                     {/* Chart */}
                     <Grid item xs={12} md={8} lg={9}>
                         <Paper className={fixedHeightPaper}>
@@ -36,12 +49,6 @@ export default function () {
                     <Grid item xs={12} md={4} lg={3}>
                         <Paper className={fixedHeightPaper}>
                             <Deposits />
-                        </Paper>
-                    </Grid>
-                    {/* Recent Orders */}
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>
-                            <Orders />
                         </Paper>
                     </Grid>
                 </Grid>
