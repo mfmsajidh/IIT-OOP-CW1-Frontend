c17707b5885c74e358b5d6d1914ccc9c8b2a5d69
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2019-12-01 10:48:58 +0530

    I know what I did !!!!

--- src/component/ListItems.js
+++ src/component/ListItems.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,47 +6,15 @@ import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
-import { Link as RouterLink } from 'react-router-dom';
-
-function ListItemLink(props) {
-    const { icon, primary, to } = props;
-
-    const renderLink = React.useMemo(
-        () =>
-            React.forwardRef((itemProps, ref) => (
-                // With react-router-dom@^6.0.0 use `ref` instead of `innerRef`
-                // See https://github.com/ReactTraining/react-router/issues/6056
-                <RouterLink to={to} {...itemProps} innerRef={ref} />
-            )),
-        [to],
-    );
-
-    return (
-        <li>
-            <ListItem button component={renderLink}>
-                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
-                <ListItemText primary={primary} />
-            </ListItem>
-        </li>
-    );
-}
-
-ListItemLink.propTypes = {
-    icon: PropTypes.element,
-    primary: PropTypes.string.isRequired,
-    to: PropTypes.string.isRequired,
-};
-
 
 export const mainListItems = (
     <>
-        {/*<ListItem button>*/}
-        {/*    <ListItemIcon>*/}
-        {/*        <DashboardIcon />*/}
-        {/*    </ListItemIcon>*/}
-        {/*    <ListItemText primary="Dashboard" />*/}
-        {/*</ListItem>*/}
-        <ListItemLink to="/home" primary={"Dashboard"} icon={<DashboardIcon/>}/>
+        <ListItem button>
+            <ListItemIcon>
+                <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+        </ListItem>
         <ListItem button>
             <ListItemIcon>
                 <EmojiTransportationIcon />
